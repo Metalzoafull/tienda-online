f2ea19da8896e4e8734d035a223fb218147a55f4
nico <juannico36@live.com.ar>
2022-01-18 22:22:45 -0300

    agregando la coneccion de las rutas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+const apiRouter = require('../routes/api')
+
 require('./db');
 
 //seteo puerto
@@ -11,6 +13,8 @@ app.set('port', process.env.PORT || 3001);
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended : false}));
 app.use(express.json());
+
+app.use('/api', apiRouter);
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
